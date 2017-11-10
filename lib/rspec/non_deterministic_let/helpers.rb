200bc0f3e07bc60963b827945ccf422bcd81fcf8
Yuta Hinokuma <yuta@abeja.asia>
2017-11-10 14:38:50 +0000

    implemented

--- lib/rspec/non_deterministic_let/helpers.rb
+++ lib/rspec/non_deterministic_let/helpers.rb
@@ -2,7 +2,29 @@ module RSpec
   module NonDeterministicLet
     module Helpers
       module ClassMethods
-        # TODO: impl
+        def nd_let_context(nd_let_name, *args, &example_group_block)
+          send(nd_let_name).each do |let_options|
+            context(let_options[:description], *args) do
+              let(nd_let_name, &let_options[:block])
+              self.module_exec(&example_group_block)
+            end
+          end
+        end
+
+        def nd_let(nd_let_name, description=nil, &let_block)
+          begin
+            let_blocks = send(nd_let_name)
+            description ||= "nd_let:#{nd_let_name} \##{let_blocks.size + 1}"
+            self.define_singleton_method(nd_let_name) do ||
+              let_blocks + [{block: let_block, description: description}]
+            end
+          rescue NoMethodError
+            description ||= "nd_let:#{nd_let_name} \##{1}"
+            self.define_singleton_method(nd_let_name) do ||
+              [{block: let_block, description: description}]
+            end
+          end
+        end
       end
       # TODO: impl
     end
